65ae62117f6f7a9eb71ffef9631455ede2da8519
vipul-bagal <vipul.bagal@somaiya.edu>
2024-08-22 18:46:44 +0530

    initial commit

---
+++ src/main/java/com/vipul/course_management_api/CourseManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.vipul.course_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication. run(CourseManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vipul/course_management_api/CourseManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipul.course_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
